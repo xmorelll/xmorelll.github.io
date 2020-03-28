eb196836a1db53cf705d567f14717a7b60d33e2f
Xavier Morell Llorens <xmorelll@uoc.edu>
2020-03-28 19:01:02 +0100

    add data to lang pages

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 // let setName = function() {
+//     //Get the name written in the input value.
 //     const name = document.getElementById('name').value||"desconocido";
+//     //Set the name in the title.
 //     document.getElementById('user-name').textContent = name;
 // };
