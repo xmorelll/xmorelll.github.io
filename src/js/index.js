3b653a242788ad362c663cd2a1ecd7105a5df8d9
Xavier Morell Llorens <xmorelll@uoc.edu>
2020-04-06 10:36:43 +0200

    javascript enhancement

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,9 @@
-// let setName = function() {
-//     //Get the name written in the input value.
-//     const name = document.getElementById('name').value||"desconocido";
-//     //Set the name in the title.
-//     document.getElementById('user-name').textContent = name;
-// };
+/**
+ * Is click at the search button
+ */
+document.getElementById("action-btn").addEventListener("click", event => {
+    //Get the name written in the input value.
+    const name = document.getElementById('name').value||"desconocido";
+    //Set the name in the title.
+    document.getElementById('user-name').textContent = name;
+});
